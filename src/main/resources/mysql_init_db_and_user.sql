996baeb68edb0ecd10a967c31d8bb071e7a4d01c
Bikash Das <bikashdaseee@gmail.com>
2019-05-20 07:46:30 +0600

    'acceptHeader' and 'userAgentHeader' column added to 'rest_activity_log' table creation SQL.

--- src/main/resources/mysql_init_db_and_user.sql
+++ src/main/resources/mysql_init_db_and_user.sql
@@ -194,6 +194,8 @@ CREATE TABLE `news_server_data_coordinator`.`rest_activity_log`
     `exceptionClassName` varchar(255)  DEFAULT NULL,
     `timeTakenMs` int(5)  NOT NULL,
     `returnedEntiryCount` int(3)  DEFAULT 0,
+    `acceptHeader` VARCHAR(45) DEFAULT NULL,
+    `userAgentHeader` VARCHAR(255) DEFAULT NULL,
     `created`    datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE = MyISAM
